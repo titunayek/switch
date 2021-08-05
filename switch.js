eafe904c795d626497036609ad2e961189655fe6
titunayek <tituroy4g@gmail.com>
2021-08-05 14:39:22 +0600

    first commit

---
+++ switch.js
@@ -0,0 +1,14 @@
+switch (color) {
+    case 'blue':
+    console.log("color is blue");
+    break;
+    case 'red':
+    console.log("color is red");
+    break;
+    case 'white':
+    console.log("color is white");
+    break;
+    default:
+    console.log("color is black");
+    break;   
+}
